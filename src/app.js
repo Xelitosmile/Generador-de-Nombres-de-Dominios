aa3506306bd37fd24aaa014aa44b237354c8848a
marcelorodriguezbolados <marcelofabianrb@gmail.com>
2022-07-21 01:08:10 +0000

    code generador

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var punto = [".com", ".net", ".us", ".io", ".etc"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let t = 0; t < adj.length; t++) {
+      for (let m = 0; m < noun.length; m++) {
+        for (let p = 0; p < punto.length; p++)
+          console.log(pronoun[i] + adj[t] + noun[m] + punto[p]);
+      }
+    }
+  }
 };
